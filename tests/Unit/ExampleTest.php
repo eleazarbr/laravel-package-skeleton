42e69e897c7fef0733ae2f2a272b5d48c3017709
Eleazar Resendez <e.resendez@outlook.com>
2024-12-20 10:32:35 -0600

    Add PHPUnit configuration and initial test cases

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Eresendez\LaravelPackageSkeleton\Tests\Unit;
+
+use Eresendez\LaravelPackageSkeleton\Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testTrueIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
