45a29ebe7604229436de319485dbd0c6b9b28354
Eleazar Resendez <e.resendez@outlook.com>
2024-12-20 11:55:21 -0600

    Add ESLint configuration for Vue.js and JavaScript linting rules

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+      browser: true,
+      es2021: true,
+  },
+  extends: [
+      "eslint:recommended",
+      "plugin:vue/vue3-recommended",
+  ],
+  parserOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+  },
+  plugins: [
+      "vue",
+  ],
+  rules: {
+    "vue/require-default-prop": "off",
+    "vue/require-prop-types": "off",
+    "comma-dangle": ["error", "always-multiline"],
+    quotes: ["error", "double", { allowTemplateLiterals: true }],
+    eqeqeq: "error",
+    "max-len": ["error", { code: 100, tabWidth: 2, ignoreComments: true }],
+    "no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
+    "vue/no-v-html": "off",
+    "no-restricted-globals": ["error", "event", "fdescribe"],
+  },
+};
